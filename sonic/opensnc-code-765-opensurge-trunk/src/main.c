e0c5474131c900930b9c1cd51dc0b964c10b8147
myeongjinkim <kiakd2@naver.com>
2015-11-19 18:20:36 +0900

    main 변수설명
    주석처리

--- sonic/opensnc-code-765-opensurge-trunk/src/main.c
+++ sonic/opensnc-code-765-opensurge-trunk/src/main.c
@@ -26,7 +26,7 @@
  * main()
  * Entry point
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) //argc 인자 , *argc[] 환경변수 설정
 {
     engine_init(argc, argv);
     engine_mainloop();
@@ -35,4 +35,3 @@ int main(int argc, char *argv[])
     return 0;
 }
 END_OF_MAIN()
-
